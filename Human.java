ae1c8fea8618922f4239ff2cd0e304f15ad71cef
Alexandr <62505110+AlDemic@users.noreply.github.com>
2020-03-24 20:07:41 +0200

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Set;
+import java.util.*;
+import java.lang.Thread;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.text.DateFormat;
+import java.util.stream.*;
+
+
+public class Human {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a+b);
+    }
+
+
+
+}
